9afc720723bb26c6aaea86217a454c4650c25313
emiliano-ma <emilianomainero@gmail.com>
2020-10-08 19:25:16 +0200

    Finish refactoring of DisplayArticles component with hooks.

--- src/components/DisplayArticles.jsx
+++ src/components/DisplayArticles.jsx
@@ -4,25 +4,29 @@ import React, { useEffect, useState } from "react";
 
 const DisplayArticles = () => {
   const [articlesArray, setArticlesArray] = useState([]);
+
   useEffect(() => {
     const getArticlesIndex = async () => {
-      const result = await getArticles;
+      const result = await getArticles();
       setArticlesArray(result);
     };
     getArticlesIndex();
   }, []);
 
-  let articlesList = articlesArray.map((article) => {
-    return (
-      <Card id={"article-" + article.id}>
-        <Image src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fwww.grade.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fplaceholder.png" />
-        <Card.Content>
-          <Card.Header>{article.title}</Card.Header>
-          <Card.Description>{article.lead}</Card.Description>
-        </Card.Content>
-      </Card>
-    );
-  });
+  let articlesList;
+  if (articlesArray.length > 0) {
+    articlesList = articlesArray.map((article) => {
+      return (
+        <Card id={"article-" + article.id}>
+          <Image src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fwww.grade.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fplaceholder.png" />
+          <Card.Content>
+            <Card.Header>{article.title}</Card.Header>
+            <Card.Description>{article.lead}</Card.Description>
+          </Card.Content>
+        </Card>
+      );
+    });
+  }
 
   return <div className="articles-container">{articlesList}</div>;
 };
