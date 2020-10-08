1fbab7d420a966baa1dc65657b0214c8ae20104f
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-08 18:38:43 +0200

    Changes class component to functional component to introduce hooks

--- src/components/DisplayArticles.jsx
+++ src/components/DisplayArticles.jsx
@@ -1,41 +1,30 @@
-import React, { Component } from "react";
 import { getArticles } from "../modules/articles";
 import { Card, Image } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
 
-class DisplayArticles extends Component {
-  state = {
-    articlesArray: [],
-  };
+const DisplayArticles = () => {
+  const [articlesArray, setArticlesArray] = useState([]);
+  useEffect(() => {
+    const getArticlesIndex = async () => {
+      const result = await getArticles;
+      setArticlesArray(result);
+    };
+    getArticlesIndex();
+  }, []);
 
-  componentDidMount = async () => {
-    let result = await getArticles();
-    this.setState({ articlesArray: result });
-  };
-
-  render() {
-    let articlesList;
-    if (this.state.articlesArray.length > 0) {
-      articlesList = this.state.articlesArray.map((article) => {
-        return (
-          <>
-            <Card id={"article-" + article.id}>
-              <Image src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fwww.grade.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fplaceholder.png" />
-              <Card.Content>
-                <Card.Header>{article.title}</Card.Header>
-                <Card.Description>{article.lead}</Card.Description>
-              </Card.Content>
-            </Card>
-          </>
-        );
-      });
-    }
-
+  let articlesList = articlesArray.map((article) => {
     return (
-      <>
-        <div className="articles-container">{articlesList}</div>
-      </>
+      <Card id={"article-" + article.id}>
+        <Image src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fwww.grade.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fplaceholder.png" />
+        <Card.Content>
+          <Card.Header>{article.title}</Card.Header>
+          <Card.Description>{article.lead}</Card.Description>
+        </Card.Content>
+      </Card>
     );
-  }
-}
+  });
+
+  return <div className="articles-container">{articlesList}</div>;
+};
 
 export default DisplayArticles;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,11 @@ import { Menu } from "semantic-ui-react";
 
 const Header = () => {
   return (
-    <>
       <Menu inverted>
         <Menu.Item>
           <h1 id="header">El Gaucho Nyheter</h1>
         </Menu.Item>
       </Menu>
-    </>
   );
 };
 
