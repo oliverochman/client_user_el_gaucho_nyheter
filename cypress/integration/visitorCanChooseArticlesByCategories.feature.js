5a0bf55ce3b16f8ec34211d04719408540cd33a6
FacundoOs <osores.facundo@gmail.com>
2020-10-13 15:04:43 +0200

    Modifyies the test to check different categories

--- cypress/integration/visitorCanChooseArticlesByCategories.feature.js
+++ cypress/integration/visitorCanChooseArticlesByCategories.feature.js
@@ -6,9 +6,25 @@ describe("Visitor can choose articles by categories", () => {
       url: "http://localhost:3000/api/v1/articles",
       response: "fixture:articles_index.json",
     });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles/?category=news",
+      response: "fixture:articles_news.json",
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles/?category=sports",
+      response: "fixture:articles_sports.json",
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles/?category=politics",
+      response: "fixture:articles_politics.json",
+    });
+
     cy.visit("/");
   });
-  context("", () => {
+  context("Visitor can see categories", () => {
     it("visitor can see different categories in the header", () => {
       cy.get("[data-cy='home']").should("contain", "Home");
       cy.get("[data-cy='news']").should("contain", "News");
@@ -18,18 +34,26 @@ describe("Visitor can choose articles by categories", () => {
   });
 
   context("successfully - News", () => {
-    beforeEach(() => {
-      cy.server();
-      cy.route({
-        method: "GET",
-        url: "http://localhost:3000/api/v1/articles/?category=news",
-        response: "fixture:articles_news.json",
-      });
-    });
     it("Visitor can see the articles in the News category", () => {
       cy.get("[data-cy='news']").click();
       cy.get("[data-cy='article-4']").within(() => {
         cy.contains("Amy Coney Barrett");
+      });
+    });
+  });
+  context("successfully - Sports", () => {
+    it("Visitor can see the articles in the News category", () => {
+      cy.get("[data-cy='sports']").click();
+      cy.get("[data-cy='article-2']").within(() => {
+        cy.contains("Former F1 driver Jolyon Palmer");
+      });
+    });
+  });
+  context("successfully - Politics", () => {
+    it("Visitor can see the articles in the News category", () => {
+      cy.get("[data-cy='politics']").click();
+      cy.get("[data-cy='article-1']").within(() => {
+        cy.contains("Brexit: Time for trade deal getting short, PM warns");
       });
     });
   });
