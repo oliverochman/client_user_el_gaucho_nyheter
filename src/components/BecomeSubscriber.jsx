6ba6c1567aa12a195fba26f684c9adcaec5c85af
sara <saralundkvist86@gmail.com>
2020-10-16 22:05:41 +0200

    setting chromeWebSecurity to false in cypress.json. Modifying BecomeSubscriber using dispatch. Creating PaymentForm component with stripe elements

--- src/components/BecomeSubscriber.jsx
+++ src/components/BecomeSubscriber.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Elements } from "react-stripe-elements";
+import PaymentForm from "./PaymentForm";
+import Subscriptions from "../modules/subscriptions";
 
 const BecomeSubscriber = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const submitPayment = async (stripeToken) => {
+    let paymentStatus = await Subscriptions.create(stripeToken);
+    if (paymentStatus.success) {
+      dispatch({
+        type: "USER_IS_SUBSCRIBER",
+        payload: {
+          role: "subscriber",
+        },
+      });
+      history.push("/", { message: paymentStatus.message });
+    }
+  };
+
   return (
-    <div>
-      <p>Become subscriber</p>
-    </div>
+    <Elements>
+      <PaymentForm submitPayment={submitPayment} />
+    </Elements>
   );
 };
 
---
+++ src/components/PaymentForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  CardExpiryElement,
+  CardCVCElement,
+  CardNumberElement,
+  injectStripe,
+} from "react-stripe-elements";
+import { Button, Form, Container } from "semantic-ui-react";
+
+const PaymentForm = (props) => {
+  const payWithStripe = async (event) => {
+    event.preventDefault();
+    let stripeResponse = await props.stripe.createToken();
+    props.submitPayment(stripeResponse.token.id);
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={payWithStripe}>
+        <div data-cy="card-number">
+          <label>Card Number</label>
+          <CardNumberElement />
+        </div>
+
+        <div data-cy="card-expiry">
+          <label>Card Expiry</label>
+          <CardExpiryElement />
+        </div>
+
+        <div data-cy="card-cvc">
+          <label>Card CVC</label>
+          <CardCVCElement />
+        </div>
+
+        <Button>Start your subscription</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default injectStripe(PaymentForm);
