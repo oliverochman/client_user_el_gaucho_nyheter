8251a083693d74f58d95661ef4d305fe9acd69c8
oliverochman <oliver.ochman@hotmail.com>
2020-10-16 17:21:02 +0200

    add stripe steps to integration test

--- cypress/integration/visitorCanBuyASubscription.feature.js
+++ cypress/integration/visitorCanBuyASubscription.feature.js
@@ -4,5 +4,34 @@ describe("User can buy a subscription", () => {
   });
   it("successfully", () => {
     cy.get('[data-cy="become-subscriber"]').click()
+    cy.wait(1000)
+
+    cy.get('[data-cy="card-number"]').within(() => {
+      cy.get('iframe[name^="__privateStripeFrame"]').then($iframe => {
+        const $body = $iframe.contents().find("body");
+        cy.wrap($body)
+          .find('input[name="cardnumber"]')
+          .type("4242424242424242", { delay: 50 });
+      });
+    })
+
+    cy.get('[data-cy="card-expiry"]').within(() => {
+      cy.get('iframe[name^="__privateStripeFrame"]').then(($iframe) => {
+        const $body = $iframe.contents().find("body");
+        cy.wrap($body).find('input[name="exp-date"]').type("1222", { delay: 10 });
+      });
+    })
+
+    cy.get('[data-cy="card-cvc"]').within(() => {
+      cy.get('iframe[name^="__privateStripeFrame"]').then(($iframe) => {
+        const $body = $iframe.contents().find("body");
+        cy.wrap($body).find('input[name="cvc"]').type("999", { delay: 10 });
+      });
+    })
+
+    cy.get('button').contains("Start your subscription").click()
+
+    cy.get("[data-cy=payment-message]").contains("Payment successfull")
+    cy.get('[data-cy="become-subscriber"]').should("not.exist")
   });
 });
