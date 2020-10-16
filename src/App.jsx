a3610f24328ce80a993579e41a128d7d35a87d96
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-16 18:41:05 +0200

    Adds BecomeSubscriber and ProtectedRoute components to make sure that only logged in users can access certain routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import DisplayArticles from "./components/DisplayArticles";
 import SingleArticle from "./components/SingleArticle";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import LoginForm from './components/LoginForm';
+import LoginForm from "./components/LoginForm";
+import ProtectedRoute from "./components/ProtectedRoute";
+import BecomeSubscriber from "./components/BecomeSubscriber";
 
 const App = () => {
   return (
@@ -13,7 +15,13 @@ const App = () => {
         <Route exact path="/" component={DisplayArticles} />
         <Route exact path="/category/:category" component={DisplayArticles} />
         <Route exact path="/articles/:id" component={SingleArticle} />
-        <Route exact path="/login" component={LoginForm} /> 
+        <Route exact path="/login" component={LoginForm} />
+
+        <ProtectedRoute path="/become-subscriber">
+          <BecomeSubscriber />
+        </ProtectedRoute>
+
+        <Route exact path="/" component={DisplayArticles} />
       </Switch>
     </>
   );
